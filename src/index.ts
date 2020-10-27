8ffff1d1abf1ec6be5a099a275089a6475a63243
isso0424 <isso04240129@gmail.com>
2020-10-28 01:10:45 +0900

    [Fix]You have to check package.json

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,20 @@ import { createConnection } from "typeorm";
 import * as plugin from "fastify-typeorm-plugin";
 import { ToDo } from "./entity/ToDo";
 
-createConnection()
-  .then((connection) => {
-    const startFetch = async () => {
-      console.log("Loading users from the database...");
-      const todos = await connection.manager.find(ToDo);
-      console.log("Loaded users: ", todos);
+const main = async () => {
+  const connection = await createConnection();
+  const startFetch = async () => {
+    console.log("Loading users from the database...");
+    const todos = await connection.manager.find(ToDo);
+    console.log("Loaded users: ", todos);
 
-      console.log("Here you can setup and run express/koa/any other framework.");
-    };
-    startFetch();
-    app.register(plugin, {
-      connection,
-    });
-    app.listen(3030);
-  })
-  .catch((error) => console.log(error));
+    console.log("Here you can setup and run express/koa/any other framework.");
+  };
+  await startFetch();
+  app.register(plugin, {
+    connection,
+  });
+  app.listen(3030);
+};
+
+main();
--- src/root.ts
+++ src/root.ts
@@ -1,6 +1,7 @@
 import fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
 import { Server, IncomingMessage, ServerResponse } from "http";
 import { ToDo } from "./entity/ToDo";
+import "fastify-typeorm-plugin";
 
 export const app: FastifyInstance<
   Server,
@@ -25,6 +26,7 @@ const opts: RouteShorthandOptions = {
 
 app.get("/", opts, async (request, reply) => {
   const todos = app.orm.getRepository(ToDo);
-  const todo = todos.findOne(1);
+  const todo = await todos.findByIds([1]);
+  console.log(todo);
   reply.code(200).send(todo);
 });
