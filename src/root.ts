da6cf48f229d577b13d69cb1b67e717840650f97
isso0424 <isso04240129@gmail.com>
2020-10-28 13:22:36 +0900

    [Add]Create POST /todo

--- src/root.ts
+++ src/root.ts
@@ -9,7 +9,7 @@ export const app: FastifyInstance<
   ServerResponse
 > = fastify();
 
-const opts: RouteShorthandOptions = {
+const getOpts: RouteShorthandOptions = {
   schema: {
     response: {
       200: {
@@ -38,10 +38,66 @@ const opts: RouteShorthandOptions = {
   },
 };
 
-app.get("/todo", opts, async (request, reply) => {
+const postOpts: RouteShorthandOptions = {
+  schema: {
+    body: {
+      title: { type: "string" },
+      description: { type: "string" },
+    },
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          created: {
+            type: "object",
+            properties: {
+              id: {
+                type: "number",
+              },
+              title: {
+                type: "string",
+              },
+              description: {
+                type: "string",
+              },
+            },
+          },
+        },
+      },
+      400: {
+        type: "string",
+      },
+    },
+  },
+};
+
+app.get("/todo", getOpts, async (request, reply) => {
   const todos = await app.orm.getRepository(ToDo).find();
   console.log(JSON.stringify(todos));
   await reply.code(200).send({
     pong: todos,
   });
+});
+
+app.post("/todo", postOpts, async (request, reply) => {
+  const parseBody = (): Record<string, string> => {
+    const body = request.body as Record<string, string>;
+    if (body.title == null || body.description == null)
+      return { error: "missing parameter" };
+    return {
+      title: body.title,
+      description: body.description,
+    };
+  };
+  const parameters = parseBody();
+  if (parameters.error != null)
+    return await reply.code(400).send(parameters.error);
+
+  const todo = new ToDo();
+  todo.title = parameters.title;
+  todo.description = parameters.description;
+
+  const repository = app.orm.getRepository(ToDo);
+  await repository.save([todo]);
+  await reply.code(200).send({ created: { ...todo } });
 });
